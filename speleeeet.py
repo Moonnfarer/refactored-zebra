4413db74061b0de462840f1d2d480c9cf65a0a51
Lukasfonsell <126168072+Lukasfonsell@users.noreply.github.com>
2023-02-23 15:12:24 +0100

    Update speleeeet.py

--- speleeeet.py
+++ speleeeet.py
@@ -47,6 +47,10 @@ class Warrior(Hero):
         print("KAPOOWW!")
         damage = self.strength*1 
         return damage 
+
+    def svordom(self):
+        damage = self.speach*1
+        return damage
         
 
 #Wizard
@@ -58,6 +62,11 @@ class Wizard(Hero):
         print("AVADA KADABRAAA!")
         damage = self.magic*1 
         return damage 
+      
+    def svordom(self):
+        damage = self.speach*1
+        return damage
+ 
 
 
 #Parent class Enemy 
@@ -76,8 +85,7 @@ class ENEMY:
         damage = self.strength/4 
         return damage 
 
-
-
+# Enemy classes 
 class enemy_warrior(ENEMY): 
     def __init__(self, name, health, strength, magic): 
         super().__init__(name, health, strength, magic)
@@ -104,13 +112,13 @@ def hero_select():
     choice = input("Välj class: \n 1. Warrior\n 2. Wizard ")
     if choice == "1": 
         hero_name = input("Namn: ")
-        player = Warrior(hero_name, 120, 50, 0) 
+        player = Warrior(hero_name, 120, 50, 0, 20) 
         player.stats() 
 
         return player
     if choice == "2": 
         hero_name = input("Namn: ")
-        player = Wizard(hero_name, 100, 15, 50) 
+        player = Wizard(hero_name, 100, 15, 50, 20) 
         player.stats() 
 
         return player
@@ -133,17 +141,9 @@ def enemy_select():
     
     return enemy 
     
-player = hero_select() 
 
+## Battle stage /// loot /// Game over
 
-
-
-
-
-
-
-
-## Battle stage och loot
 
 def game_over(): 
     print("GAME OVER")
@@ -154,32 +154,33 @@ def game_over():
         print("Tack för att du spelade!")
         
 
-
-
 def loot():
+    global player 
     loot = ["cider", "databasteknik-boken", "stringtrosor"]
     lootchance = random.randint(0,2)
     lootDrop = loot[lootchance]
     if loot == "cider": 
         player.strength += 15
+        print("Din strength är nu ", player.strength)
     if loot == "databasteknik-boken": 
-        player.health += 20 
+        player.magic += 20 
+        print("Din magic är nu ", player.magic)
     if loot == "stringtrosor": 
         player.health += 15
+        print("Ditt liv är nu ", player.health)
     return lootDrop
 
-def svordom():
-    damage = self.speach/4 
-    return damage
- 
+
+player = hero_select() 
 
 
 def battlestate():
-    
+    global player  
     enemy = enemy_select()
     print("en vild ", enemy.name, "står framför dig!")
     print("du har 3 alternativ...")
-    while enemy.health >= 0:
+    
+    while enemy.health > 0:
         if player.health <= 0:  
             game_over()
             break 
@@ -191,11 +192,13 @@ def battlestate():
             hitchance = random.randint(0,10)
             if hitchance > 3:
                 
-                enemy.health = enemy.health - player.attack()  
+                enemy.health = enemy.health - player.attack() 
+                player.strength -= 10
+                player.magic -= 10
                 print("du träffade mitt på kinden och hon har nu", enemy.health, " hp " )
 
                 if enemy.health > 0 :
-                    player.health = player.health - (enemy.attack())
+                    player.health = player.health - enemy.attack()
                     print(enemy.name, "blir arg och slår tillbaka och du har", player.health - enemy.attack())
 
                 else: 
@@ -214,34 +217,47 @@ def battlestate():
                 #break
 
 
-        elif choice == "2":
-                    svordomen = svordom()
-                    print("Du slänger ur dig '", svordomen, "' i hopp att skada ", enemy.name)
-                    if svordomen == "logikfitta":
-                        print("det var elakt men ", enemy.name, "inser att det stämmer och tar inte illa upp!")
-                        player.health = player.health - enemy.svordom()
-                        print(enemy.name, "blir arg och slår tillbaka och du har", player.health, "HP kvar")
-                    elif svordomen == "knarklangare":
-                        enemy.health = enemy.health - player.speach
-                        print("Ajajaj det var elakt ", enemy.name, " tar illa upp och har nu ", enemy.health, "HP kvar.")
-                        if enemy.health > 0 :
-                            player.health = player.health - (enemy.svordom())
-                            print(enemy.name, "blir arg och slår tillbaka och du har", player.health, "HP kvar")
+        elif choice == "2":                 
+            hitchance = random.randint(0,3)
 
-                        else: 
-                            if enemy.name == "Vera":
-                                enemy.health = 20
-                            elif enemy.name == "Thea":
-                                enemy.health = 20
-                            elif enemy.name == "Frida":
-                                enemy.health = 20
+            if hitchance == 0:
+                svordomen = "logikfitta!"
+                print("Du slänger ur dig '", svordomen, "' i hopp att skada ", enemy.name) 
+                print("det var elakt men ", enemy.name, "inser att det stämmer och tar inte illa upp!")
+                player.health = player.health - enemy.strength
+                print(enemy.name, "blir arg och slår tillbaka och du har", player.health, "HP kvar")
+                
 
-                            
-                            print("Du har besegrat ", enemy.name)
-                            print("åååh det droppades något!")
-                            lootDrop = loot()
-                            print("Du fick en ", lootDrop, "....")
-                            break
+            elif hitchance > 0: 
+                svordomen = "knarklangare" 
+                print("Du slänger ur dig '", svordomen, "' i hopp att skada ", enemy.name) 
+                enemy.health = enemy.health - player.svordom() 
+                print("Ajajaj det var elakt ", enemy.name, " tar illa upp och har nu ", enemy.health, "HP kvar.")
+                
+                if enemy.health > 0 :
+                    player.health = player.health - enemy.strength
+                    print(enemy.name, "blir arg och slår tillbaka och du har", player.health, "HP kvar")
+
+
+                ### Reset Stats
+                else: 
+                    if enemy.name == "Vera":
+                        enemy.health = 70
+                    elif enemy.name == "Thea":
+                        enemy.health = 70
+                    elif enemy.name == "Frida":
+                        enemy.health = 70
+
+                    player = Wizard(player.name, 100, 15, 50) 
+                    player = Warrior(player.name, 120, 50, 0) 
+
+                    print("Du har besegrat ", enemy.name)
+                    print("åååh det droppades något!")
+                    lootDrop = loot()
+                    print("Du fick en ", lootDrop, "....")
+
+                    player 
+                    break
 
         elif choice == "3":
             trillchans = random.randint(1,10)
@@ -256,6 +272,10 @@ def battlestate():
 
 
 ### Lägg till story 
+
+
+#battlestate() 
+
 
 def main(): 
     battlestate() 
